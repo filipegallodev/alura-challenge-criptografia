b4cd4f0601c55b528651e97e81b2101806b9f42a
Filipe <filipegallodev@hotmail.com>
2022-08-11 16:51:47 -0300

    funcao descriptografar pronta / refatoracao do codigo

--- scripts/script.js
+++ scripts/script.js
@@ -1,74 +1,45 @@
-var entradaTexto = document.querySelector(".criptografia-entrada");
+var entradaTexto = document.querySelector("#criptografia-entrada");
+var resultadoCriptografia = document.querySelector("#criptografia-resultado");
 var botaoCriptografa = document.querySelector(".botao-criptografa");
+var botaoDescriptografa = document.querySelector(".botao-descriptografa");
 
-var textoPuro = entradaTexto.value;
-var textoCriptografado = "";
+var textoOriginal = entradaTexto.value;
+var textoCriptografado, textoDescriptografado = "";
 
 function criptografaTexto() {
 
-   for (var posicao = 0; posicao < textoPuro.length; posicao++) {
+   textoCriptografado = trocaLetras(textoOriginal);
 
-      var letraOriginal = textoPuro[posicao];
-      var letraConvertida = converteLetra(letraOriginal);
-      textoCriptografado = textoCriptografado.concat(letraConvertida);
-   }
-   alert(textoCriptografado);
-   alert(textoPuro);
-}
-
-function converteLetra(letraOriginal) {
-
-   if (letraOriginal == "a") {
-
-      return "ai";
-   }
-   else if (letraOriginal == "e") {
-
-      return "enter";
-   }
-   else if (letraOriginal == "i") {
-
-      return "imes";
-   }
-   else if (letraOriginal == "o") {
-
-      return "ober";
-   }
-   else if (letraOriginal == "u") {
-
-      return "ufat";
-   }
-   else {
-
-      return letraOriginal;
-   }
+   resultadoCriptografia.value = textoCriptografado;
 }
 
 function descriptografaTexto() {
 
-   
+   textoDescriptografado = retornaLetra(textoCriptografado);
+
+   resultadoCriptografia.value = textoDescriptografado;
 }
 
-function desconverteLetra() {
+function trocaLetras(textoOriginal) {
 
+   return textoOriginal
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+}
 
+function retornaLetra(palavraCriptografada) {
+
+   return palavraCriptografada
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
 }
 
 botaoCriptografa.onclick = criptografaTexto;
 
-
-/*
-   textoPuro = textoPuro.replace('a', 'ai');
-   textoPuro = textoPuro.replace('e', 'enter');
-   textoPuro = textoPuro.replace('i', 'imes');
-   textoPuro = textoPuro.replace('o', 'ober');
-   textoPuro = textoPuro.replace('u', 'ufat');
-*/
-
-/*
-textoPuro = textoPuro.replace('ai', 'a');
-textoPuro = textoPuro.replace('enter', 'e');
-textoPuro = textoPuro.replace('imes', 'i');
-textoPuro = textoPuro.replace('ober', 'o');
-textoPuro = textoPuro.replace('ufat', 'u');
-*/
+botaoDescriptografa.onclick = descriptografaTexto;
